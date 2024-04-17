6bf51723e28f903c69bf85fb9fc5e0a9561db6cc
ctcakip1 <tinigaming058@gmail.com>
2024-04-17 13:44:27 +0700

    video 46

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -9,26 +9,31 @@ import vn.hoidanit.laptopshop.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    @GetMapping("/")
-
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "tuananh.html";
     }
 }
+
+// @RestController
+// public class UserController {
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("/")
+
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+// }
