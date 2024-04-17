5a3646b5142b7ebf8562544bf0ef3fbeb25eddde
ctcakip1 <tinigaming058@gmail.com>
2024-04-17 21:10:30 +0700

    video 60

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -23,15 +24,13 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("tuananh", test);
+        model.addAttribute("tuananh", "test");
         model.addAttribute("haha", "from controller with model");
         return "hello";
     }
 
     @RequestMapping(value = "/admin/user")
     public String getUserPage(Model model) {
-        String test = this.userService.handleHello();
         model.addAttribute("newUser", new User());
         model.addAttribute("haha", "from controller with model");
         return "admin/user/create";
@@ -40,7 +39,7 @@ public class UserController {
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User tuananh) {
         System.out.println("run here " + tuananh);
-
+        this.userService.hanldeSaveUser(tuananh);
         return "hello";
     }
 
