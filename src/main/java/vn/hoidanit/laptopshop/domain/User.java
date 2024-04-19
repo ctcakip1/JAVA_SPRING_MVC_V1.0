9121bb79783db5d3f45487e11d09a823a23b5f02
ctcakip1 <tinigaming058@gmail.com>
2024-04-19 22:12:12 +0700

    video 79

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,9 +1,14 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,7 +24,14 @@ public class User {
     private String address;
     private String phone;
     private String avatar;
-    //roleId
+    // roleId
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+    // orderId
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+
     public long getId() {
         return id;
     }
